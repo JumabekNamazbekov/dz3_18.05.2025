e787993b41135c93655af35d48d6e404ee700213
Jumabek Namazbekov <m.jumabek@list.ru>
2025-05-20 11:11:08 +0700

    Добавьте файлы проекта.

---
+++ dz218/dz218.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+   
+    int* ptr = new int;
+
+   
+    cout << "Введите число: ";
+    cin >> *ptr; 
+
+   
+    *ptr = (*ptr) * (*ptr);
+    cout << "Квадрат числа: " << *ptr << endl;
+
+   
+    delete ptr;
+
+    return 0;
+}
